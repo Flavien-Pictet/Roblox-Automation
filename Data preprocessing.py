e9320d0b6ec6b8d9bcb611bdd6230adfa4fdd96f
Flavien Pictet <flavien@pictet.net>
2024-02-02 15:56:18 +0100

    push

--- Data preprocessing.py
+++ Data preprocessing.py
@@ -1,14 +1,8 @@
 import pandas as pd
 
-# Lire le fichier CSV d'entrée
-input_csv_file = "twitter_links.csv"
-df = pd.read_csv(input_csv_file, header=None, names=["data"])  # Remplacez "data" par le nom de la colonne que vous souhaitez dédupliquer
+def remove_duplicates(input_file, output_file):
+    df = pd.read_csv(input_file, header=None, names=["Twitter", "Discord"])
+    df_cleaned = df.drop_duplicates()
+    df_cleaned.to_csv(output_file, index=False, header=False)
 
-# Supprimer les doublons
-df_cleaned = df.drop_duplicates(subset=["data"])  # Remplacez "data" par le nom de la colonne que vous souhaitez dédupliquer
-
-# Enregistrer les données nettoyées dans un nouveau fichier CSV
-output_csv_file = "clean_data.csv"
-df_cleaned.to_csv(output_csv_file, index=False, header=False)
-
-print(f"Données nettoyées enregistrées dans '{output_csv_file}'.")
+remove_duplicates("social_links.csv", "clean_social_links.csv")
